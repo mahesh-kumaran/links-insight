921ab056ab562c36cbb15194c8a30324d89e793d
mahesht10 <mahesh@autonom8.com>
2019-11-13 15:07:26 +0530

    fix: link preview undefined error

--- link_insight/parser.py
+++ link_insight/parser.py
@@ -3,8 +3,8 @@ from webpreview import OpenGraph
 import requests, os
 
 
-# LINK_PREVIEW_ACCESS_KEY = "5dc93469844a9ca355f31b8e517abc70bf477ca77f4ce"
-# LINK_PREVIEW_URL = "http://api.linkpreview.net/"
+# LINK_PREVIEW_ACCESS_KEY="5dc93469844a9ca355f31b8e517abc70bf477ca77f4ce"
+# LINK_PREVIEW_URL="http://api.linkpreview.net/"
 
 LINK_PREVIEW_ACCESS_KEY = os.environ["LINK_PREVIEW_ACCESS_KEY"]
 LINK_PREVIEW_URL = os.environ["LINK_PREVIEW_URL"]
@@ -22,6 +22,7 @@ class LinkParser():
             if response.status_code == 200: 
                 response_data = response.json()
                 response_data['parser'] = "link_preview_parser"
+                del response_data['url']
                 return response_data
             else:
                 return { "comment" : "Unable to Access Link Preview Service"}
@@ -32,13 +33,13 @@ class LinkParser():
 
         try :
             url_info = {}
-            url_parser['parser'] = "html_parser" 
+            url_info["parser"] = "html_parser" 
             title, description, image = web_preview(self.url, parser="html.parser")
 
-            url_info["title"] = title, 
-            url_info["description"] = description
-            url_info["image"] = image 
-            url_info["parser"] = "html_parser" 
+            url_info['title'] = title 
+            url_info['description'] = description
+            url_info['image'] = image 
+            
 
             return url_info
         except Exception as e:
@@ -52,10 +53,9 @@ class LinkParser():
             url_info['parser'] = "open_graph_parser" 
             og = OpenGraph(self.url, ["og:title", "og:description", "og:image"])
 
-            url_info["title"] = og.title, 
+            url_info["title"] = og.title
             url_info["description"] = og.description
             url_info["image"] = og.image 
-            url_info["parser"] = "open_graph_parser" 
 
             return url_info
         except Exception as e:
@@ -73,8 +73,8 @@ def parse_link_information(url):
     # If HTML Web parser also fails, Then try using Preview Link    
     if 'title' in og_response and og_response['title'] == None:
         html_response =  link_parser.parse_using_web()
-        if html_response['title'] == None: 
-            link_preview_response = parse_using_link_preview()
+        if html_response['title'] == None : 
+            link_preview_response = link_parser.parse_using_link_preview()
             return link_preview_response
         else: 
             return html_response
